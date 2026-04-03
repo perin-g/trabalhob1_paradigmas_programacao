b40ad1aa197a053e69da6e93f26490bdb3c622bd
paolamachado2056 <paolamachado2056@gmail.com>
2026-04-03 17:24:46 -0300

    implementei algumas funcoes ai de arqueiro

--- src/Classes/Arqueiro.java
+++ src/Classes/Arqueiro.java
@@ -6,6 +6,17 @@ public class Arqueiro extends Personagem implements Especial {
         private int  destreza;
         private int flechas;
 
+        public Arqueiro (String nome, int destreza, int flechas){
+            super(nome);
+            this.destreza = destreza;
+            this.flechas = flechas;
+        }
+        public Arqueiro ( String nome, int hp, int hpMax, int defesaBase, int destreza, int flechas){
+            super(nome, hp, hpMax, defesaBase);
+            this.destreza = destreza;
+            this.flechas = flechas;
+        }
+
 
     public void setDestreza(int destreza) {
         this.destreza = destreza;
@@ -21,10 +32,32 @@ public class Arqueiro extends Personagem implements Especial {
     public void setFlechas(int flechas) {
         this.flechas = flechas;
     }
+
+    @Override
+    protected int getPoderAtaque() {
+        return 0;
+    }
+
+    @Override
     public void exibirStatus() {
         System.out.println("Nome: " + this.getNome() + " | HP atual: " + this.getHp() + " | HP máximo: " + this.getHpMax() +
                 " | Defesa base:" + this.getDefesaBase() + " | Flechas: " + this.getFlechas() +
                 " | Destreza: " + this.getDestreza());
+    }
+    @Override
+    public void usarHabilidadeEspecial(Personagem alvo){
+            int danoAtaque;
+            if( flechas > 0){
+                danoAtaque = destreza * 2;
+                alvo.receberDano(danoAtaque);
+                this.flechas--;
+                System.out.println("Habilidade especial usada com sucesso, voce causou "
+                        + danoAtaque + " ao inimigo. | voce possui "
+                        + this.flechas + " flechas.");
+            } else {
+                System.out.println("não é possivel realizar a habilidade especial sem flechas.");
+
+            }
     }
 
 }
