37e635c454e26adc5be9d9c91028b04ab036091b
paolamachado2056 <paolamachado2056@gmail.com>
2026-04-03 16:07:37 -0300

    exibir status, implementei propriedades arqueiro, criei classe clerigo

--- src/Classes/Arqueiro.java
+++ src/Classes/Arqueiro.java
@@ -3,5 +3,28 @@ package Classes;
 import Interfaces.Especial;
 
 public class Arqueiro extends Personagem implements Especial {
+        private int  destreza;
+        private int flechas;
+
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public int getFlechas() {
+        return flechas;
+    }
+
+    public void setFlechas(int flechas) {
+        this.flechas = flechas;
+    }
+    public void exibirStatus() {
+        System.out.println("Nome: " + this.getNome() + " | HP atual: " + this.getHp() + " | HP máximo: " + this.getHpMax() +
+                " | Defesa base:" + this.getDefesaBase() + " | Flechas: " + this.getFlechas() +
+                " | Destreza: " + this.getDestreza());
+    }
 
 }
---
+++ src/Classes/Clerigo.java
@@ -0,0 +1,8 @@
+package Classes;
+
+import Interfaces.Especial;
+
+public class Clerigo extends Personagem implements Especial {
+    private int fe;
+    private int oracoes;
+}
