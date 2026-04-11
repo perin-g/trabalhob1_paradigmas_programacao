f6fa61ed365afe55b9f458fc0fb30d9d012b3cdb
Gabriel Perin <gabriel.2015012222@alunocesul.com>
2026-04-11 18:06:07 -0300

    Implementada menu de criação de personagens e lógica de turnos

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ void main() {
 
     Scanner sc = new Scanner(System.in);
     List<Personagem> time = new ArrayList<>();
-    int opcao = -1;
 
     System.out.println("Masmorras & Dragões - uma aventura épica.");
     System.out.println("Vamos iniciar nossa aventura criando nossos heróis.");
@@ -35,20 +34,113 @@ void main() {
         }
     }
 
-    do {
-        System.out.println("opcoes de jogo:");
-        System.out.println("digite a opcao desejada para essa rodada.");
-        System.out.println("1-atacar");
-        System.out.println("2- usar habilidade especial");
-        System.out.println("3- curar um aliado");
-        System.out.println("4-passar a vez");
 
-    } while ();
 
+    boolean turno = true;
+    while (turno) {
+        int contadorTurnos = 1;
+        System.out.println("--- " + contadorTurnos + "º TURNO ---");
+        System.out.println("Lista de heróis:");
+        int aux = 0;
+        for (Personagem p : time) {
+            aux += 1;
+            System.out.println(aux);
+            p.exibirStatus();
+        }
+
+        System.out.println("Selecione o personagem que deve agir agora");
+        int personagem = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Selecione o alvo");
+        int alvoAtaque = Integer.parseInt(sc.nextLine());
+
+        if (personagem >= 0 && personagem < time.size() && alvoAtaque >= 0 & alvoAtaque <= time.size()) {
+            Personagem atacante = time.get(personagem);
+            Personagem alvo = time.get(alvoAtaque);
+
+            System.out.println("Ações do personagem:");
+            System.out.println("1 - Atacar");
+            System.out.println("2 - Usar habilidade' especial");
+            System.out.println("3 - Curar um aliado");
+            System.out.println("4 - Passar a vez");
+            System.out.println("Qual ação deseja fazer?");
+            int acao = Integer.parseInt(sc.nextLine());
+
+            switch (acao) {
+                case 1:
+                    if (atacante instanceof Guerreiro) {
+                        Guerreiro g = (Guerreiro) atacante;
+
+                        System.out.println("Usar Grito de Guerra? (s/n) \nGrito de guerra: Adiciona +5 no ataque, só pode ser usado uma vez por combate.");
+                        String usarGrito = sc.nextLine().toLowerCase();
+
+                        if (usarGrito.equals("s")) {
+                            g.gritoDeGuerra(alvo);
+                        } else {
+                            g.atacar(alvo);
+                        }
+                    } else if (atacante instanceof Mago) {
+                        Mago m = (Mago) atacante;
+
+                        System.out.println("Usar Raio Congelante? (s/n) \nRaio de gelo: Consome 20 de mana, causa 12 de dano e imprime um efeito de “congelamento” no alvo.");
+                        String raioCongelante = sc.nextLine().toLowerCase();
+                        if (raioCongelante.equals("s")) {
+                            m.raioCongelante(alvo);
+                        } else {
+                            m.atacar(alvo);
+                        }
+                    } else if (atacante instanceof Arqueiro) {
+                        Arqueiro a = (Arqueiro) atacante;
+
+                        System.out.println("Usar Flehca Venenosa? (s/n) \nFlecha Venenosa: causa dano imediato (igual a destreza) e aplica envenenamento");
+                        String flechaVenenosa = sc.nextLine().toLowerCase();
+
+                        if (flechaVenenosa.equals("s")) {
+                            a.flechaVenenosa(alvo);
+                        } else {
+                            a.atacar(alvo);
+                        }
+                    } else {
+                        atacante.atacar(alvo);
+                    }
+                    break;
+                case 2:
+                    atacante.usarHabilidadeEspecial(alvo);
+                    break;
+                case 3:
+                    if (atacante instanceof Clerigo) {
+                        Clerigo c = (Clerigo) atacante;
+                        c.curar(alvo);
+                    } else {
+                        System.out.println("Sua classe não pode curar!");
+                        break;
+                    }
+                case 4:
+                    System.out.println("Pulando turno...");
+                    break;
+                default:
+                    System.out.println("Opção inválida, pulando turno...");
+                    break;
+            }
+
+            if (atacante.getHp() == 0) {
+                System.out.println("O herói " + atacante.getNome() + " acaba de morrer, combate encerrado.");
+                turno = false;
+                break;
+            } else if (alvo.getHp() == 0) {
+                System.out.println("O herói " + atacante.getNome() + " acaba de morrer, combate encerrado.");
+                turno = false;
+                break;
+            }
+        }
+    }
+
+    System.out.println("Encerrando a sessão de Masmorras & Dragões... ");
+    sc.close();
 }
 
 private static Personagem criarPersonagem(String nome, int classe, Scanner sc) {
-//    Deixado setado valores padrão para facilitar o teste e correção, mas desenvolvida a lógica para pegar os dados dos usuário.
+//    Deixado setado valores padrão para facilitar o teste e correção, mas desenvolvida a lógica para pegar os dados do usuário.
 //    System.out.println("Informe seu HP máximo: ");
 //    int hp = Integer.parseInt(sc.nextLine());
 //    System.out.println("Informe sua defesa: ");
